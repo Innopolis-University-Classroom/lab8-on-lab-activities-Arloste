80341b142fab9dbcf94ed5d5a3a8ee3ad6b577cb
Arloste <88305350+Arloste@users.noreply.github.com>
2022-03-21 17:27:08 +0300

    Update Human.java

--- src/com/muwaffaq/innopolis/solid/homework/Human.java
+++ src/com/muwaffaq/innopolis/solid/homework/Human.java
@@ -5,61 +5,49 @@ import java.util.List;
 
 // lab work
 // This Class violates all SOLID Principles, fix it in a Logical way
-public class Human implements Needs {
+public class Human implements Language
+{
+    private Name name;
+    private PersonalNeeds pNeeds;
+    private Business business;
 
-    private String name;
-    private String nickname;
-    private double salary;
-    private List<String> hobbies;
-
-    enum Languages {Arabic, English, Japanese}
-
-    String sayHello(Languages languages) {
-        if (languages == Languages.Arabic)
-            return "مرحبا";
-        return "Hello";
+    String sayHello() {
+        return Languages.sayHello();
     }
-
-    void calculateTax(int percentage) {
-        salary = salary * percentage;
-    }
-
+    
+    @Override
     int addHobby(String hobby) {
         hobbies.add(hobby);
         return hobbies.size();
     }
 
+    @Override
     void creatNickName(String postFix) {
         nickname = name.concat(postFix);
     }
-
+    
+    @Override
+    void calculateTax(int percentage) {
+        salary = salary * percentage;
+    }
+    
     @Override
     public void pray() {
     }
 
     @Override
     public void playSports() {
-
     }
 
     @Override
     public void getMarried() {
-
     }
 
     @Override
     public void ownCompany() {
-
     }
 
     @Override
     public void becomeEmployee() {
-
     }
-
-    public static void main(String[] args) {
-        Human human = new Human();
-        human.sayHello(Languages.Arabic);
-    }
-
 }
